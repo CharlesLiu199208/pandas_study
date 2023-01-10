6f43580c60b7dff36931640398030fe07f688c20
charles <charelesliu1992@gmail.com>
2023-01-10 21:18:50 +0800

    day6

---
+++ BasicStudy/随机函数.py
@@ -0,0 +1,78 @@
+import random
+import string
+
+ran1 = random.randint(1,6)
+ran2 = random.randrange(start=1,stop=50,step=3)
+
+'''
+string.digits #0-9数字
+string.printable # 数字
+.lower 强制转小写
+.upper 强制转大写
+'''
+list_ch = string.printable
+
+
+# print(ran1)
+# print(ran2)
+# print(list_ch)
+def GePass(nu,ty):
+    '''
+    :param nu: 密码位数
+    :param ty: 1：纯数字；2：数字加字幕；3数字加字母加字符
+    :return: 密码
+    '''
+
+    if int(ty) == 3:
+        li = string.printable.replace(' ', '').replace('\r', '')
+    elif int(ty) == 1:
+        li = string.digits
+    elif int(ty) == 2:
+        li = str(string.digits) + string.ascii_letters
+    # print(li)
+    cho = 'y'
+    while cho == 'y':
+        passcode = ''
+        for i in range(int(nu)):
+            passcode += random.choice(li)
+            # print(x)
+        print(passcode)
+        cho = input('是否重新生成密码，如果需要重新生成请点击‘y’，退出点击其它:').lower()
+    else:
+        print('请保管好您的密码:{}'.format(passcode))
+    return passcode
+
+def GePass_m(nu,ty, som):
+    '''
+    :param nu: 密码位数
+    :param ty: 1：纯数字；2：数字加字幕；3数字加字母加字符
+    :param som: 生成密码梳理
+    :return: 密码
+    '''
+
+    if int(ty) == 3:
+        li = string.printable.replace(' ', '').replace('\r', '')
+    elif int(ty) == 1:
+        li = string.digits
+    elif int(ty) == 2:
+        li = str(string.digits) + string.ascii_letters
+    # print(li)
+    cho = 1
+    passcode_list = []
+    while cho > int(som):
+        passcode = ''
+        for i in range(int(nu)):
+            passcode += random.choice(li)
+            print(passcode)
+        passcode_list.append(passcode)
+        cho += 1
+    return passcode_list
+
+
+num = input('请输入你想生成密码的位数:')
+type = input('请输入你想生成密码的类型，1：纯数字；2：数字加字幕；3数字加字母加字符：')
+sum = input('请输入你想生成密码的个数：')
+if int(sum) > 1 :
+    GePass_m(num,type,sum)
+else:
+ GePass(num,type)
--- day5.py
+++ day5.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 # 测试表格
-des = r'E:\Develop\Project\test_file'
+des = r'./TestFile'
 des_books = des + '\\' + 'day5.xlsx'
+
+def add_2(x):
+    return x + 2
 
 # 读取一个表格
 books = pd.read_excel(des_books, index_col='ID')
@@ -11,7 +14,16 @@ books = pd.read_excel(des_books, index_col='ID')
 # books['Price'] = books['ListPrice']*books['Discount']
 
 # 循环写法,从其中的某一段技术需要用到这个循环for i in range（5,16）
-for i in books.index:
-    books['Price'].at[i] = books['ListPrice'].at[i] * books['Discount'].at[i]
+# for i in books.index:
+#     books['Price'].at[i] = books['ListPrice'].at[i] * books['Discount'].at[i]
+
+# 自加,列中所有单元格加2
+# books['ListPrice'] = books['ListPrice'] + 2
+
+# 可以使用.apply 里运行函数对单元格进行修改,apply中只填函数名称
+# books['ListPrice'] = books['ListPrice'].apply(add_2)
+
+# 也可以写成lambda表达式
+books['ListPrice'] = books['ListPrice'].apply(lambda x: x+2)
 
 print(books)
